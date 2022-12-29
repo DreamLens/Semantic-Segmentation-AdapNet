63f109340e820fe43e531edb0d03ca39de64eebc
DreamLens <DreamLen98@proton.me>
2022-12-29 14:30:53 +0000

    Small fix [skip hazardlib] [demos]

--- evaluate.py
+++ evaluate.py
@@ -9,4 +9,33 @@
  (at your option) any later version.
 
  This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRAN
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.'''
+
+import argparse
+import datetime
+import importlib
+import os
+import numpy as np
+import tensorflow as tf
+import yaml
+from dataset.helper import *
+
+PARSER = argparse.ArgumentParser()
+PARSER.add_argument('-c', '--config', default='config/cityscapes_test.config')
+
+def test_func(config):
+    os.environ['CUDA_VISIBLE_DEVICES'] = config['gpu_id']
+    module = importlib.import_module('models.' + config['model'])
+    model_func = getattr(module, config['model'])
+    data_list, iterator = get_test_data(config)
+    resnet_name = 'resnet_v1_50'
+
+    with tf.variable_scope(resnet_name):
+        model = model_func(num_classes=config['num_classes'], training=False)
+        images_pl = tf.placeholder(tf.float32, [None, config['height'], config['width'], 3])
+        model.build_graph(images_pl)
