5ed1880fcf35e31cdcbdfe3649682772be2cd261
DreamLens <DreamLen98@proton.me>
2023-01-13 08:14:35 +0000

    removing unnecessary file

--- models/AdapNet.py
+++ models/AdapNet.py
@@ -8,4 +8,30 @@
  the Free Software Foundation, either version 3 of the License, or
  (at your option) any later version.
 
- This program is distributed in the hope that it 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.'''
+
+import tensorflow as tf
+import network_base
+
+class AdapNet(network_base.Network):
+    def __init__(self, num_classes=12, learning_rate=0.001, float_type=tf.float32, weight_decay=0.0005,
+                 decay_steps=30000, power=0.9, training=True, ignore_label=True, global_step=0,
+                 has_aux_loss=False):
+        super(AdapNet, self).__init__()
+        self.num_classes = num_classes
+        self.learning_rate = learning_rate
+        self.weight_decay = weight_decay
+        self.initializer = 'xavier'
+        self.has_aux_loss = has_aux_loss
+        self.float_type = float_type
+        self.power = power
+        self.decay_steps = decay_steps
+        self.training = training
+        self.bn_decay_ = 0.99
+        self.resi
